af65c1c2805612efc58658e74b03f0c8813c5eb2
Vincent RODRIGUEZ <vincent.rlr@gmail.com>
2022-12-08 18:37:53 +0900

    FInished the printing blob function from a list, adding test cases

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
+import numpy as np
 from sudokuslvr.sudoku import Grid
 
+
+
+
+# creating a grid and print the blob for display
+testing_list = np.zeros((81,), dtype=int)
+print(testing_list)
+
 x = Grid()
-x.add_blob([3,4,5,6,7])
+x.add_blob(testing_list)
 x.print_blob()
